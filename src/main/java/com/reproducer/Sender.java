8e79a2c211fe4e2418a6ae0a10797fb9c2f9b437
mathiastoserve <mathias4u@gmx.net>
2015-04-28 23:32:52 +0200

    Set send timeout

--- src/main/java/com/reproducer/Sender.java
+++ src/main/java/com/reproducer/Sender.java
@@ -11,7 +11,7 @@ public class Sender extends AbstractVerticle {
   public void start() {
     vertx.setPeriodic(Main.SENDER_INTERVAL, l -> {
       DeliveryOptions options = new DeliveryOptions();
-      options.setSendTimeout(Main.SENDER_INTERVAL);
+      options.setSendTimeout(Main.SEND_TIMEOUT);
       vertx.eventBus().send("inbound.something", "{\"data\": \"Hello\"}", options, ar -> {
         if(ar.succeeded()){
           System.out.println("Sender: Ping-Pong completed");
