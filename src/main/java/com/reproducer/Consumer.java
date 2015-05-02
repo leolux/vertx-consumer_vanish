6ccd4810f8b6b569ae1cbd2f4b5b3c8a4d67b5c7
mathiastoserve <mathias4u@gmx.net>
2015-05-02 21:44:43 +0200

    time messurement

--- src/main/java/com/reproducer/Consumer.java
+++ src/main/java/com/reproducer/Consumer.java
@@ -3,6 +3,7 @@ package com.reproducer;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerOptions;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.apex.Router;
 import io.vertx.ext.apex.handler.sockjs.BridgeOptions;
 import io.vertx.ext.apex.handler.sockjs.PermittedOptions;
@@ -15,7 +16,12 @@ public class Consumer extends AbstractVerticle {
 
   public void start() {
     vertx.eventBus().consumer("inbound.something", msg -> {
+      JsonObject jsonObj = (JsonObject) msg.body();
+      final int count = jsonObj.getInteger("count");
+      System.out.println("Count: "+count + " Consumer thread: " + Thread.currentThread().getId());
+      
       vertx.executeBlocking(blocking -> {
+        System.out.println("Count: "+count + " Blocking thread: " + Thread.currentThread().getId());
         try {
           Thread.sleep(Main.REPLY_DELAY);
         } catch (Exception e) {
@@ -23,6 +29,7 @@ public class Consumer extends AbstractVerticle {
         }
         blocking.complete();
       }, ar -> {
+        System.out.println("Count: "+count + " Reply thread: " + Thread.currentThread().getId());
         msg.reply("[\"whatever\"]");
       });
     });
--- src/main/java/com/reproducer/Sender.java
+++ src/main/java/com/reproducer/Sender.java
@@ -1,7 +1,10 @@
 package com.reproducer;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.eventbus.DeliveryOptions;
+import io.vertx.core.json.JsonObject;
 
 /**
  *
@@ -9,16 +12,30 @@ import io.vertx.core.eventbus.DeliveryOptions;
 public class Sender extends AbstractVerticle {
 
   public void start() {
-    vertx.setPeriodic(Main.SENDER_INTERVAL, l -> {
-      DeliveryOptions options = new DeliveryOptions();
-      options.setSendTimeout(Main.SEND_TIMEOUT);
-      vertx.eventBus().send("inbound.something", "{\"data\": \"Hello\"}", options, ar -> {
-        if(ar.succeeded()){
-          System.out.println("Sender: Ping-Pong completed");
-        }else{
-          System.out.println("Sender: Consumer vanished?");
-        }
-      });
-    });
+    AtomicInteger i = new AtomicInteger(0);
+    vertx.setPeriodic(
+        Main.SENDER_INTERVAL,
+        l -> {
+          final int count = i.incrementAndGet();
+          final long before = System.currentTimeMillis();
+          System.out.println("Count: "+count + " Sender thread: " + Thread.currentThread().getId());
+          JsonObject msg = new JsonObject();
+          msg.put("count", count);
+          vertx.eventBus().send(
+              "inbound.something",
+              msg,
+              new DeliveryOptions().setSendTimeout(Main.SEND_TIMEOUT),
+              ar -> {
+                final long after = System.currentTimeMillis();
+                final long respTime = after - before;
+                if (ar.succeeded()) {
+                  System.out.println("Count: "+count + " Sender: Ping-Pong completed. Response time: "
+                      + respTime);
+                } else {
+                  System.out.println("Count: "+count + " Sender: Consumer vanished? Response time: "
+                      + respTime);
+                }
+              });
+        });
   }
 }
